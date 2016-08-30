ea88617d5e9e779f92f2c04de48488d111655dcc
Johan Girod <johangirod@gmail.com>
2016-08-30 14:00:20 +0200

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE OverloadedStrings, PatternGuards #-}
+
 module Main where
 
-import Lib
+import           Network.Linklater
+import           Network.Wai.Handler.Warp (run)
+import           Data.Text.IO (readFile, writeFile)
+import           Data.Text (Text)
+import qualified Data.Text as T
+
+type Token = Text
+
+sayHi :: Config -> Command -> IO Text
+sayHi config (Command { commandName = "sayHi" }) = return "Hi!"
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn ("+ Listening on port " <> show port)
+    run port (slashSimple $ sayHi configIO)
+    where
+        configIO :: IO Config
+        configIO =
+            (Config . T.filter (/= '\n')) <$> readFile "token"
+        port = 3335
